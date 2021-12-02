ef678e1784ed27a139d172e47dd555bb2a8ba954
sgtpeppers <92605951+AtlasNavigator@users.noreply.github.com>
2021-12-02 21:07:36 +0000

    Initial commit

--- index.js
+++ index.js
@@ -1,13 +1,31 @@
 const readLine = require('readline-sync');
 
-// Example 1
-function nameSwapper(first, last){
-  let name = last +", "+ first;
-  return name;
+// // Example 1
+// function nameSwapper(first, last){
+//   let name = last +", "+ first;
+//   return name;
+// }
+
+// let firstName = readLine.question("Enter first name: ");
+// let lastName = readLine.question("Enter last name: ");
+// console.log(nameSwapper(firstName, lastName));
+
+// // Example 2
+// function generateNum(){
+//   let num1 = readLine.question("Enter a digit: ");
+//   let num2 = readLine.question("Enter another digit: ");
+//   return num1+num2;
+// }
+// console.log(generateNum());
+// console.log(generateNum());
+
+// Example 3
+function avgTemp(){
+  let temp1 = readLine.question("Day 1: ");
+  let temp2 = readLine.question("Day 2: ");
+  let temp3 = readLine.question("Day 3: ");
+  let temp4 = readLine.question("Day 4: ");
+  let temp5 = readLine.question("Day 5: ");
+  let temp6 = readLine.question("Day 6: ");
+  let temp7 = readLine.question("Day 7: ");
 }
-
-let firstName = readLine.question("Enter first name: ");
-let lastName = readLine.question("Enter last name: ");
-console.log(nameSwapper(firstName, lastName));
-
-// Example 2
